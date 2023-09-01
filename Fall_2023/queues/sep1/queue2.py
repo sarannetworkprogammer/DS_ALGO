95b8d5b3b6850860cff8458bbdacf2e9ede705f9
saran <saranchowdari@gmail.com>
2023-09-01 10:59:34 -0500

    queue

---
+++ Fall_2023/queues/sep1/queue2.py
@@ -0,0 +1,18 @@
+
+"First in first out"
+queue = []
+
+queue.append("a")
+queue.append("b")
+queue.append("c")
+queue.append("d")
+
+print(f"Initial queue ={queue}")
+
+
+print(f"Elements dequeued from queue")
+
+print(queue.pop(0))
+
+
+print(f"after rmeoving elements= {queue}")
