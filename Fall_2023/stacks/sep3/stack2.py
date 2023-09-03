df2a2c84b636792b5fe0ddb56a11536c38a35977
saran <saranchowdari@gmail.com>
2023-09-03 12:59:34 -0500

    stack

---
+++ Fall_2023/stacks/sep3/stack2.py
@@ -0,0 +1,24 @@
+A = [4, 5, 2, 10, 8]
+
+stack =[]
+
+n = len(A)
+
+ans =[0]*n
+ans[n-1] = -1
+stack.append(A[n-1])
+
+
+for i in range(n-2,-1):
+
+    while len(stack) != 0 and stack[-1] <= A[i]:
+        stack.pop()
+    
+    
+    if stack:
+        ans[i] = stack[-1]
+    else:
+        ans[i] =-1
+
+    stack.append(A[i])
+print(ans)
