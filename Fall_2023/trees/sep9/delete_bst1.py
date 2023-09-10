94b764ea3611ad1327433e957d1d2842f27fc47b
saran <saranchowdari@gmail.com>
2023-09-09 20:49:21 -0500

    tree

---
+++ Fall_2023/trees/sep9/delete_bst1.py
@@ -0,0 +1,18 @@
+class TreeNode:
+
+    def __init__(self,val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+
+def delete_node(key):
+
+    pass
+
+delete_node(2)
