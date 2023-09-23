42003da82efe915f373c8cf6be7929444dc0c265
saran <saranchowdari@gmail.com>
2023-09-22 20:22:54 -0500

    heap

--- Fall_2023/trees/sep22/binaryheap2.py
+++ Fall_2023/trees/sep22/binaryheap2.py
@@ -16,4 +16,21 @@ def peekofHeap(rootNode):
     else:
         return rootNode.customList[1]
 
+def sizeofHeap(rootNode):
+    if not rootNode:
+        return
+    else:
+        return rootNode.
+
+def levelordertraversal(rootNode):
+    if not rootNode:
+        return
+    else:
+        for i in range(1,rootNode.heapSize+1):
+            print(rootNode.customList[i])
+
+
+
+
 newBinaryHeap = Heap(5)
+print(sizeofHeap(newBinaryHeap))
