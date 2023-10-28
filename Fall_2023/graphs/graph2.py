e9346c256dfc93c52af152b3183e37ee5c670bcc
saran <saranchowdari@gmail.com>
2023-10-28 15:53:37 -0500

    graph2

---
+++ Fall_2023/graphs/graph2.py
@@ -0,0 +1,26 @@
+class Graph:
+
+    def __init__(self):
+
+        self.adjacency_list = {}
+
+    def add_vertex(self,vertex):
+
+        if vertex not in self.adjacency_list.keys():
+            self.adjacency_list[vertex] = []
+            return True
+        return False
+    
+
+    def print_graph(self):
+
+        for vertex in self.adjacency_list:
+            print(vertex, ":", self.adjacency_list[vertex])
+
+
+custom_graph = Graph()
+
+custom_graph.add_vertex("A")
+custom_graph.print_graph()
+
+    
