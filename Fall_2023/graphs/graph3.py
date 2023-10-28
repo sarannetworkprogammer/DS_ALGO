e72766e243fbc0cf4579202938ac74a319d2ea90
saran <saranchowdari@gmail.com>
2023-10-28 16:05:13 -0500

    graph

---
+++ Fall_2023/graphs/graph3.py
@@ -0,0 +1,46 @@
+class Graph:
+
+    def __init__(self):
+
+        self.adjacency_list = {}
+
+    def add_vertex(self,vertex):
+
+        if vertex not in self.adjacency_list.keys():
+            self.adjacency_list[vertex] = []
+            return True
+        return False
+    
+
+    def print_graph(self):
+
+        for vertex in self.adjacency_list:
+            print(vertex, ":", self.adjacency_list[vertex])
+
+
+    def add_edge(self, vertex1, vertex2):
+
+
+        if vertex1 in self.adjacency_list.keys() and vertex2 in self.adjacency_list.keys():
+
+            self.adjacency_list[vertex1].append(vertex2)
+            self.adjacency_list[vertex2].append(vertex1)
+            return True
+
+        return False
+
+
+
+
+custom_graph = Graph()
+
+custom_graph.add_vertex("A")
+
+custom_graph.add_vertex("B")
+
+custom_graph.add_edge("A", "B")
+custom_graph.print_graph()
+
+
+
+    
---
+++ Fall_2023/graphs/remove_edge.py
@@ -0,0 +1,43 @@
+class Graph:
+
+    def __init__(self):
+
+        self.adjacency_list = {}
+
+    def add_vertex(self,vertex):
+
+        if vertex not in self.adjacency_list.keys():
+            self.adjacency_list[vertex] = []
+            return True
+        return False
+    
+
+    def print_graph(self):
+
+        for vertex in self.adjacency_list:
+            print(vertex, ":", self.adjacency_list[vertex])
+
+
+    def add_edge(self, vertex1, vertex2):
+
+
+        if vertex1 in self.adjacency_list.keys() and vertex2 in self.adjacency_list.keys():
+
+            self.adjacency_list[vertex1].append(vertex2)
+            self.adjacency_list[vertex2].append(vertex1)
+            return True
+
+        return False
+
+    def remove_edge(self,vertex1, vertex2):
+
+        if vertex1 in self.adjacency_list.keys() and vertex2 in self.adjacency_list.keys():
+
+            self.adjacency_list[vertex1].remove(vertex2)
+            self.adjacency_list[vertex2].remove(vertex1)
+
+            return True 
+
+        return False
+
+
