69841e7f8de5950de05a7ff84528a1a491b808bc
saran <saranchowdari@gmail.com>
2023-10-28 16:08:06 -0500

    graph

--- Fall_2023/graphs/remove_edge.py
+++ Fall_2023/graphs/remove_edge.py
@@ -41,3 +41,20 @@ class Graph:
         return False
 
 
+my_graph = Graph()
+
+my_graph.add_vertex("A")
+my_graph.add_vertex("B")
+my_graph.add_vertex("C")
+
+my_graph.add_edge("A","B")
+my_graph.add_edge("A","C")
+my_graph.add_edge("B","C")
+my_graph.print_graph()
+
+
+
+my_graph.remove_edge("A", "C")
+
+print("after removing edge")
+my_graph.print_graph()
